104724dd125db780843793567f094ef4b30833ab
Phil McCown <pjlmac@gmail.com>
2017-09-10 11:32:38 -0400

    Answer to #4

--- homework.sh
+++ homework.sh
@@ -1,5 +1,5 @@
 for homework in *.pdb
 do
-echo $homework >> homework3.txt
-sort -n -k5 $homework | head -n 1 | cut -f 2-3 >> homework3.txt
+echo $homework >> homework4.txt
+sort -n -k5 "$@" | head -n 1 | cut -f 2-3 >> homework4.txt
 done
